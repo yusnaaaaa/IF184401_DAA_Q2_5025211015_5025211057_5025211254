9dc98a3c3658c1e981cc7dadf745d476fa805359
Salsabila Fatma Aripa <114417418+bilaaripa@users.noreply.github.com>
2023-05-18 16:27:03 +0700

    fix : simplify result checker

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -16,27 +16,22 @@ void gameBoard(){
 }
 
 char result(){
-    if (arr[0][0] == arr[1][0] && arr[1][0] == arr[2][0] && arr[2][0]!='.'){
-            return arr[0][0];
+    for (int i = 0; i < 3; i++){
+        if (arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2] && arr[i][2]!='.'){
+            return arr[i][0];
+        }
     }
-    if (arr[0][1] == arr[1][1] && arr[1][1] == arr[2][1] && arr[2][1]!='.'){
-            return arr[0][1];
+
+    for (int i = 0; i < 3; i++){
+        if (arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i] && arr[2][i]!='.'){
+            return arr[0][i];
+        }
     }
-    if (arr[0][2] == arr[1][2] && arr[1][2] == arr[2][2] && arr[2][2]!='.'){
-            return arr[0][2];
-    }
-    if (arr[0][0] == arr[0][1] && arr[0][1] == arr[0][2] && arr[0][2]!='.'){
-            return arr[0][0];
-    }
-    if (arr[1][0] == arr[1][1] && arr[1][1] == arr[1][2] && arr[1][2]!='.'){
-            return arr[1][0];
-    }
-    if (arr[2][0] == arr[2][1] && arr[2][1] == arr[2][2] && arr[2][2]!='.'){
-            return arr[2][0];
-    }
+
     if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[2][2]!='.'){
         return arr[0][0];
     }
+
     if (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[2][0]!='.'){
         return arr[0][2];
     }
@@ -54,6 +49,7 @@ char result(){
     if(cek) return 'T';
     return '-';
 }
+
 
 int minimax(int depth, bool isMaximizing){
 
